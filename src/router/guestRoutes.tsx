ad1c4a0a3093e4bd4c4eb2f11591307a92a0beaa
Phil <philip.a.oshea@gmail.com>
2024-12-31 00:41:17 +0000

    Assistant checkpoint: Fix imports and exports in guestRoutes
    Assistant generated file changes:
    - src/router/guestRoutes.tsx: Fix all imports and exports in guestRoutes
    
    ---
    
    User prompt:
    
    can you not fix all the pages .. it is too slow one by one

--- src/router/guestRoutes.tsx
+++ src/router/guestRoutes.tsx
@@ -1,11 +1,12 @@
+
 import { RouteObject } from "react-router-dom";
+import { PublicRoute } from "../components/auth/ProtectedRoute";
 import Jobs from "../pages/jobs/Jobs";
 import Resources from "../pages/resources/Resources";
 import SuccessStories from "../pages/success-stories/SuccessStories";
 import Analytics from "../pages/partners/Analytics";
 import Apprenticeships from "../pages/partners/Apprenticeships";
 import Mentorship from "../pages/partners/Mentorship";
-import { PublicRoute } from "../components/auth/ProtectedRoute";
 import Landing from "../pages/Landing";
 
 const guestRoutes: RouteObject[] = [
